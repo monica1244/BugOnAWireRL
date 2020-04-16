accb1f99f05fc618388b21368561809442c5355a
Nihal Singh <nihal.111@gmail.com>
2020-04-15 20:37:56 -0400

    Added frames.py

--- main.py
+++ main.py
@@ -6,20 +6,6 @@ import time
 from pymouse import PyMouse
 from pykeyboard import PyKeyboard
 
-# def screen_record(): 
-#     last_time = time.time()
-#     while(True):
-#         # 800x600 windowed mode for GTA 5, at the top left position of your main screen.
-#         # 40 px accounts for title bar. 
-#         printscreen =  np.array(ImageGrab.grab(bbox=(100,200,750,650)))
-#         print('loop took {} seconds'.format(time.time()-last_time))
-#         last_time = time.time()
-#         cv2.imshow('window',cv2.cvtColor(printscreen, cv2.COLOR_BGR2RGB))
-#         if cv2.waitKey(25) & 0xFF == ord('q'):
-#             cv2.destroyAllWindows()
-#             break
-
-# screen_record()
 
 for i in list(range(4))[::-1]:
     print(i+1)
