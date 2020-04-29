6921d82a7b01a5c471ef7e68db8fb9709a5b9116
Nihal Singh <nihal.111@gmail.com>
2020-04-28 23:05:40 -0400

    Flappy birds environment added

---
+++ flappy_birds_environment.py
@@ -0,0 +1,182 @@
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+import numpy as np
+from PIL import ImageGrab
+# import pyscreenshot as ImageGrab
+import cv2
+
+import numpy as np
+import time
+
+import time
+from pymouse import PyMouse
+from pykeyboard import PyKeyboard
+
+import os
+import torch
+from getkeys import key_check
+
+
+# Set TOP_LEFT here as per the right pixel by checking mouse position
+# when running this script
+TOP_LEFT = (107, 197)
+HEIGHT, WIDTH = 639, 479
+SCREEN = (TOP_LEFT[0], TOP_LEFT[1], TOP_LEFT[0] + WIDTH, TOP_LEFT[1] + HEIGHT)
+# BROWN in RGB
+BROWN = [232, 97, 1]
+WHITE = [255, 255, 255]
+GAME_OVER_PIXEL = (567 - TOP_LEFT[1], 216 - TOP_LEFT[0])
+NEW_GAME_PIXEL = (TOP_LEFT[0] + 110, TOP_LEFT[1] + 370)
+FRAMES = 1
+# 4 Actions: Nothing, UP, LEFT, RIGHT
+N_ACTIONS = 2
+RESOLUTION = 84
+
+mouse = PyMouse()
+keyboard = PyKeyboard()
+
+def init_env():
+    os.chdir("C:\Program Files\Mozilla Firefox")
+    os.system('firefox.exe --new-tab "https://flappybird.io/"')
+    time.sleep(10)
+    os.chdir(r"C:\Users\nihal\github\bugOnAWireRL")
+
+
+def generate_state(frame, dim=RESOLUTION, channels=1):
+    state = cv2.resize(frame, (dim, dim)) / 255.0
+    # if np.random.random_sample() > 0.5:
+    #     mouse.click(TOP_LEFT[0] + 10, TOP_LEFT[1] + 10, 1)
+    #     cv2.imshow('img', state)
+    #     cv2.moveWindow('img', 1400, 30)
+    #     cv2.waitKey(0)
+    return state[None, :, :].astype(np.float32)
+
+def start_new_game():
+    mouse.click(NEW_GAME_PIXEL[0], NEW_GAME_PIXEL[1], 1)
+    time.sleep(0.1)
+    mouse.click(NEW_GAME_PIXEL[0], NEW_GAME_PIXEL[1], 1)
+    frame = pre_process(np.array(ImageGrab.grab(bbox=SCREEN)))
+    state = generate_state(frame)
+    return state
+
+def is_game_over(frame, pre_processed_frame):
+    '''
+    Checks pixel GAME_OVER_PIXEL corresponding to a patch in the
+    restart button that is BROWN
+
+    If that pixel has value == BROWN
+    return True i.e. game is over
+    '''
+    if list(frame[GAME_OVER_PIXEL]) == BROWN or np.sum(pre_processed_frame) == 0:
+        return True
+    else:
+        return False
+
+
+def pre_process(img):
+    ## Filter Bird
+    mask = get_mask_for_colors(img, [[234, 80, 64],
+                                            [212, 191, 39],
+                                            [235, 252, 221],
+                                            [221, 226, 177],
+                                            [228, 129, 22]])
+
+    ## Filter Pipes
+    mask += get_mask_for_colors(img, [[85, 128, 34],
+                                              [228, 253, 139],
+                                              [155, 227, 89],
+                                              [115, 191, 46],
+                                              [157, 230, 90],
+                                              [115, 189, 46],
+                                              [117, 192, 47]])
+    return mask
+
+
+def get_reward_and_next_state(action, dim=84):
+    if action == 0:
+        # NO ACTION
+        # time.sleep(0.1)
+        pass
+    elif action == 1:
+        # mouse click
+        mouse.click(TOP_LEFT[0], TOP_LEFT[1], 1)
+        # time.sleep(0.1)
+
+    frame = np.array(ImageGrab.grab(bbox=SCREEN))
+    pre_processed_frame = pre_process(frame)
+    if is_game_over(frame, pre_processed_frame):
+        state = None
+        frame_diff = None
+        reward = -2
+    else:
+        reward = pipe_reward(pre_processed_frame) + bird_in_view_reward(frame)
+        state = generate_state(pre_processed_frame)
+    return reward, state
+
+def pipe_reward(pre_processed_frame):
+    # wire = pre_processed_frame[:, 305-TOP_LEFT[0]:340-TOP_LEFT[0]]
+    # cv2.imshow('img', wire)
+    if pre_processed_frame[(750 - TOP_LEFT[1], 340 - TOP_LEFT[0])] > 0 and \
+            pre_processed_frame[(205 - TOP_LEFT[1], 340 - TOP_LEFT[0])] > 0:
+        return 1
+    else:
+        return 0
+
+def bird_in_view_reward(frame):
+    mask = get_mask_for_colors(frame, [[234, 80, 64],
+                                        [212, 191, 39],
+                                        [235, 252, 221],
+                                        [221, 226, 177],
+                                        [228, 129, 22]])
+    if np.sum(mask) > 0:
+        return 0
+    else:
+        return -1
+
+
+def get_mask_for_colors(img, colors):
+    '''
+    Takes RGB colors and returns a mask filtering those colors.
+    '''
+    mask = np.zeros((img.shape[0], img.shape[1]))
+    for color in colors:
+        color = np.array(color)
+        color_lo = color - np.array([1, 1, 1])
+        color_hi = color + np.array([1, 1, 1])
+        mask += cv2.inRange(img, color_lo, color_hi)
+    return mask
+
+
+
+def screen_record():
+    last_time = time.time()
+    # start_new_game()
+    while(True):
+        printscreen = np.array(ImageGrab.grab(bbox=SCREEN))
+        print('loop took {:.2f} seconds'.format(time.time() - last_time))
+        last_time = time.time()
+
+        m_pos = mouse.position()
+        color = printscreen[(np.clip(m_pos[1] - TOP_LEFT[1], 0, HEIGHT - 1),
+            np.clip(m_pos[0] - TOP_LEFT[0], 0, WIDTH - 1))]
+
+        pre_processed_frame = pre_process(printscreen)
+        state = generate_state(pre_processed_frame)
+        # print("Mouse position: {} Color: {}".format(m_pos, color))
+        # print("Game Over? " + str(is_game_over(img, pre_processed_frame)))
+
+        ## PLAY with random action
+        # get_reward_and_next_state(np.random.randint(0, 2))
+        # if is_game_over(pre_processed_frame):
+        #     break
+
+        cv2.imshow('window', pre_processed_frame)
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break
+
+
+if __name__ == '__main__':
+    screen_record()
---
+++ train_flappy.py
@@ -0,0 +1,283 @@
+import random
+import os
+import time
+import numpy as np
+import torch, torchvision
+import torch.optim as optim
+import matplotlib.pyplot as plt
+from itertools import count
+from collections import namedtuple
+import torch.nn.functional as F
+
+from dqn_flappy import DQN
+from replay_memory import ReplayMemory
+import cv2
+import pickle
+import signal
+
+from flappy_birds_environment import init_env, start_new_game, \
+    get_reward_and_next_state, N_ACTIONS, RESOLUTION
+
+np.random.seed(111)
+torch.manual_seed(111)
+
+BATCH_SIZE = 32
+TRAIN_ITERATIONS = 100
+DQFD_TRAIN_ITERATIONS = 100
+LEARNING_RATE = 1e-4
+GAMMA = 0.999
+EPS_START = 0.1
+EPS_END = 0.0001
+EPS_DECAY = 200
+TARGET_UPDATE = 1
+SAVE_MODEL = 20
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
+
+
+num_episodes = 500
+episode_durations = []
+res_path = "results/"
+model_checkpoint_path = "model-egreedy-chk.pt"
+replay_buffer_path = "replay-egreedy.pkl"
+
+steps_done = 0
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+policy_net = DQN(RESOLUTION, RESOLUTION, N_ACTIONS).to(device)
+target_net = DQN(RESOLUTION, RESOLUTION, N_ACTIONS).to(device)
+
+memory = ReplayMemory(10000)
+
+# If loading a saved model
+# print("Loading Model and Replay buffer")
+# if os.path.exists(model_checkpoint_path):
+#     policy_net.load_state_dict(torch.load(model_checkpoint_path))
+# with open(replay_buffer_path, 'rb') as input:
+#     memory.set_memory(pickle.load(input))
+
+optimizer = optim.Adam(policy_net.parameters(), lr=LEARNING_RATE)
+target_net.load_state_dict(policy_net.state_dict())
+target_net.eval()
+
+
+def keyboardInterruptHandler(signal, frame):
+    print("KeyboardInterrupt has been caught. Saving plot...")
+    plot_durations()
+    exit(0)
+
+
+# signal.signal(signal.SIGINT, keyboardInterruptHandler)
+
+
+def plot_durations():
+    """Function to plot and store the durations of each episode
+
+    Args: i_episode: episode number used in the image name
+
+    """
+
+    plt.figure(2)
+    plt.clf()
+    durations_t = torch.tensor(episode_durations, dtype=torch.float)
+    plt.title('Training...')
+    plt.xlabel('Episode')
+    plt.ylabel('Duration')
+    plt.plot(durations_t.numpy())
+    if not os.path.exists(res_path):
+        print("doesnt existtt")
+        os.makedirs(res_path)
+    try:
+        plt.savefig(res_path + "duration_plot_{}.png".format(time.strftime("%Y%m%d-%H%M%S")))
+    except:
+        print("Unable to save plot")
+    # Take 100 episode averages and plot them too
+    # if len(durations_t) >= 100:
+    #     means = durations_t.unfold(0, 100, 1).mean(1).view(-1)
+    #     means = torch.cat((torch.zeros(99), means))
+    #     plt.plot(means.numpy())
+    #     plt.savefig(res_path+"duration_plot_{}.png".format(i_episode))
+
+    plt.pause(0.001)  # pause a bit so that plots are updated
+    time.sleep(1)
+
+def optimize_model(memory):
+    """Function for gradient updates
+
+    In this function we sample from memory(ReplayMemory), use the policy_net to
+    get the state_action_values and the target net and the next_states to compute
+    the expected_state_action_values and use huber loss to update the weights.
+
+    """
+    if len(memory) < BATCH_SIZE:
+        return 0
+    transitions = memory.sample(BATCH_SIZE)
+    # Transpose the batch (see https://stackoverflow.com/a/19343/3343043 for
+    # detailed explanation). This converts batch-array of Transitions
+    # to Transition of batch-arrays.
+    batch = Transition(*zip(*transitions))
+
+    # Compute a mask of non-final states and concatenate the batch elements
+    # (a final state would've been the one after which simulation ended)
+    non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
+                                          batch.next_state)), device=device, dtype=torch.bool)
+    non_final_next_states = torch.cat([torch.Tensor(s) for s in batch.next_state
+                                                if s is not None]).to(device)
+    state_batch = torch.cat([torch.Tensor(s) for s in batch.state]).to(device)
+    action_batch = torch.cat([torch.LongTensor([[s]]) for s in batch.action]).to(device)
+    reward_batch = torch.cat([torch.Tensor([s]) for s in batch.reward]).to(device)
+
+    # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
+    # columns of actions taken. These are the actions which would've been taken
+    # for each batch state according to policy_net
+    state_action_values = policy_net(state_batch).gather(1, action_batch)
+
+    # Compute V(s_{t+1}) for all next states.
+    # Expected values of actions for non_final_next_states are computed based
+    # on the "older" target_net; selecting their best reward with max(1)[0].
+    # This is merged based on the mask, such that we'll have either the expected
+    # state value or 0 in case the state was final.
+    next_state_values = torch.zeros(BATCH_SIZE, device=device)
+    next_state_values[non_final_mask] = target_net(non_final_next_states).max(1)[0].detach()
+    # Compute the expected Q values
+    expected_state_action_values = (next_state_values * GAMMA) + reward_batch
+
+    # Compute Huber loss
+    loss = F.smooth_l1_loss(state_action_values, expected_state_action_values.unsqueeze(1))
+
+    # Optimize the model
+    optimizer.zero_grad()
+    loss.backward()
+    for param in policy_net.parameters():
+        param.grad.data.clamp_(-1, 1)
+    optimizer.step()
+    return loss.item()
+
+
+def select_action(state, eps=None):
+    """Function for select an action givena state
+
+    In this function we select a random sample variable and compare it with epsilon
+    threshold to decide whether to explore or exploit.
+
+    """
+    global steps_done
+    sample = random.random()
+    if eps is not None:
+        eps_threshold = eps
+    else:
+        eps_threshold = EPS_END + (EPS_START - EPS_END) * \
+            np.exp(-1. * steps_done / EPS_DECAY)
+    if sample > eps_threshold:
+        with torch.no_grad():
+            state = torch.FloatTensor(state)
+            action = policy_net(state.to(device)).max(1)[1].view(1, 1).item()
+            return action, (sample, eps_threshold)
+    else:
+        action = random.randrange(N_ACTIONS)
+        return action, None
+
+
+def main():
+    """Main Training Loop"""
+    # init_env()
+    # Warm up the policy network
+    select_action(np.zeros((1, 4, RESOLUTION, RESOLUTION)))
+
+    for i_episode in range(num_episodes):
+        # Initialize the environment and state
+        time.sleep(0.5)
+        frame = start_new_game()
+        state = np.concatenate(tuple(frame for _ in range(4)))[None, :, :, :]
+
+        last_time = time.time()
+        print("Game Start Time: {}".format(time.strftime("%Y/%m/%d-%H:%M:%S")))
+        for t in count():
+            last_time = time.time()
+            action, network_action = select_action(state)
+
+            reward, next_frame = get_reward_and_next_state(action)
+
+            if next_frame is None:
+                next_state = None
+            else:
+                next_state = np.concatenate((state[0, 1:, :, :], next_frame))[None, :, :, :]
+
+            if network_action is not None:
+                print("t:{}\t time:{:.2f}\t reward:{}\t action:{}\t NETWORK {:.2f}/{:.2f}".format(t, time.time() - last_time, reward, action, network_action[0], network_action[1]))
+            else:
+                print("t:{}\t time:{:.2f}\t reward:{}\t action:{}\t".format(t, time.time() - last_time, reward, action))
+            # if reward > 1:
+            #     print("REWARD -- {}".format(reward))
+            # Store the transition in memory
+            memory.push(state,
+                action,
+                next_state,
+                reward)
+
+            # Move to the next state
+            state = next_state
+
+            if next_frame is None:
+                episode_durations.append(t + 1)
+                break
+
+
+        # Update the target network, copying all weights and biases in DQN
+        if i_episode % TARGET_UPDATE == 0:
+            target_net.load_state_dict(policy_net.state_dict())
+
+        if i_episode % SAVE_MODEL == 0:
+            torch.save(policy_net.state_dict(), model_checkpoint_path)
+            print("Saving replays at episode {}".format(i_episode))
+            with open(replay_buffer_path, 'wb') as output:
+                pickle.dump(memory.get_memory(), output)
+            eval()
+
+        print("Episode {}/{} -- Duration: {}".format(i_episode, num_episodes, t+1))
+        print("Memory Size: {}".format(len(memory.get_memory())))
+
+        print("Optimizing model")
+        global steps_done
+        steps_done += 1
+
+        # optim_time = time.time()
+        # loss = 0
+        # for _ in range(DQFD_TRAIN_ITERATIONS):
+        #     loss += optimize_model(dqfd_memory)
+        # print("DQFD\t BATCH_SIZE:{}\t TRAIN_ITERATIONS:{}\t Time:{:.2f}\t Avg Loss:{:.2f}".format(
+        #     BATCH_SIZE, TRAIN_ITERATIONS, time.time() - optim_time, loss/TRAIN_ITERATIONS))
+
+        optim_time = time.time()
+        loss = 0
+        for _ in range(TRAIN_ITERATIONS):
+            loss += optimize_model(memory)
+        print("eGREEDY\t BATCH_SIZE:{}\t TRAIN_ITERATIONS:{}\t Time:{:.2f}\t Avg Loss:{:.2f}".format(
+            BATCH_SIZE, TRAIN_ITERATIONS, time.time() - optim_time, loss/TRAIN_ITERATIONS))
+
+
+def eval():
+    print("\nStarting Eval")
+    time.sleep(2)
+    frame = start_new_game()
+    state = np.concatenate(tuple(frame for _ in range(4)))[None, :, :, :]
+    with torch.no_grad():
+        last_time = time.time()
+        for t in count():
+            last_time = time.time()
+            action, network_action = select_action(state, eps=0) # Choose best action
+            reward, next_frame = get_reward_and_next_state(action)
+            if next_frame is None:
+                next_state = None
+            else:
+                next_state = np.concatenate((state[0, 1:, :, :], next_frame))[None, :, :, :]
+            print("t:{}\t time:{:.2f}\t reward:{}\t action:{}\t NETWORK {:.2f}/{:.2f}".format(t, time.time() - last_time, reward, action, network_action[0], network_action[1]))
+            # Move to the next state
+            state = next_state
+            if next_frame is None:
+                break
+        print("Eval Duration: {}".format(t))
+
+if __name__ == "__main__":
+    main()
+    eval()
+    plot_durations()
